9fc9fa7043bab7e97df596e660e2467b6117c61b
mvrz <msv930@gmail.com>
2025-11-13 11:29:42 -0600

    Fixed useEffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,35 @@
-// create your App component here
+import { useEffect, useState} from 'react'
+function App() {
+    const [image, switchImage] = useState("https://dog.ceo/api/breeds/image/random")
+    const [loading, switchLoading] = useState(true)
+
+
+    function changeImage() {
+        switchLoading(true) //loading state resets every time the button is clicked
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Failed to fetch image") //.catch will catch the error
+            }
+            return response.json()
+        })
+        .then(data => {
+            switchImage(data.message) //updating the image state
+            switchLoading(false) //tje API request is successful, so the loading message should not appear
+        })
+        .catch(e => console.log(e))
+        
+    }
+
+    useEffect(changeImage, [])
+    
+    return (
+        <div>
+            {loading ? <p>Loading...</p> : ""}
+            <img src={image} alt="A Random Dog"/>
+            <button onClick={changeImage}>Click to change picture</button>
+        </div>
+    )
+}
+
+export default App
